2486e2da65335a9aeafac9ef62e6695dd5ba9576
Elyse Burmester <burmesterelyse@gmail.com>
2019-06-07 14:34:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,3 +41,21 @@ class Passenger {
   }
 }
 
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
+    store.trips.push(this);
+  }
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+}
